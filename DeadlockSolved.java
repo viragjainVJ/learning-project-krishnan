edd912408bf8c38424b9979f3ec73be4b3a7c84e
Ramrk11 <39863783+Ramrk11@users.noreply.github.com>
2018-11-09 11:43:11 +0530

    Create DeadlockSolved.java

---
+++ DeadlockSolved.java
@@ -0,0 +1,44 @@
+public class ThreadDemo1 {
+   public static Object Lock1 = new Object();
+   public static Object Lock2 = new Object();
+   
+   public static void main(String args[]) {
+	   TD1 T1 = new TD1();
+	   TD2 T2 = new TD2();
+      T1.start();
+      T2.start();
+   }
+   
+   private static class TD1 extends Thread {
+	   public void run() {
+	         synchronized (Lock1) {
+	            System.out.println("Thread 1: Holding lock 1...");
+	            
+	            try {
+	               Thread.sleep(10);
+	            } catch (InterruptedException e) {}
+	            System.out.println("Thread 1: Waiting for lock 2...");
+	            
+	            synchronized (Lock2) {
+	               System.out.println("Thread 1: Holding lock 1 & 2...");
+	            }
+	         }
+	      }
+   }
+   private static class TD2 extends Thread {
+	   public void run() {
+	         synchronized (Lock1) {
+	            System.out.println("Thread 2: Holding lock 1...");
+	           
+	            try {
+	               Thread.sleep(10);
+	            } catch (InterruptedException e) {}
+	            System.out.println("Thread 2: Waiting for lock 2...");
+	            
+	            synchronized (Lock2) {
+	               System.out.println("Thread 2: Holding lock 1 & 2...");
+	            }
+	         }
+	      }
+	   } 
+}
